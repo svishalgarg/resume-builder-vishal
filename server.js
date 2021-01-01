01476ed3c87ff0ef5ee7620e3b91f3b454dc7bbe
Vishal Garg <vishalgargabcd@gmail.com>
2021-01-01 14:32:04 +0530

    new changes

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 let express = require('express');
+var bodyParser = require('body-parser');
 let fs = require('fs');
 let pdf = require('html-pdf');
 let htmlDocx = require('html-docx-js');
 
+const server_url = "https://resume-builder-node.herokuapp.com";
+//const server_url = "http://127.0.0.1:8080";
+
 let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
 app.use(function(req,res,next){
     res.setHeader('Access-Control-Allow-Origin','*');
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     //res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
@@ -20,14 +27,22 @@ app.get('/',function(req,res){
     res.sendFile(__dirname + "/" + "index.html" );
 })
 
-app.get('/generate_resume',function(request,response){
-    let name = request.query.name;
-    let email = request.query.email;
-    let designation = request.query.designation;
-    let objective = request.query.objective;
-    let template = request.query.template;
+app.post('/generate_resume',function(req,res){
+    let name = req.body.name;
+    let email = req.body.email;
+    let designation = req.body.designation;
+    let objective = req.body.objective;
+    let template = req.body.template;
 
-    let file_name = "resume.html";
+    let font_size = req.body.font_size || "14";
+    let font_style = req.body.font_style || '"Times New Roman", Times, serif';
+    let heading_size = req.body.heading_size || "20";
+    let section_spacing = req.body.section_spacing || "10";
+    let paragraph_spacing = req.body.paragraph_spacing || "normal";
+    let line_spacing = req.body.line_spacing || "normal";
+    let text_color = req.body.text_color;
+
+    let file_name = "template1.html";
     if(template == "template1"){
         file_name = "template1.html";
     }
@@ -42,18 +57,26 @@ app.get('/generate_resume',function(request,response){
     html = html.replace("{{user_designation}}",designation);
     html = html.replace("{{user_objective}}",objective);
 
+    html = html.replace("{{font_size}}",font_size);
+    html = html.replace("{{heading_size}}",heading_size);
+    html = html.replace("{{font_style}}",font_style);
+    html = html.replace("{{text_color}}",text_color);
+    html = html.replace("{{line_spacing}}",line_spacing);
+    html = html.replace("{{section_spacing}}",section_spacing);
+    html = html.replace("{{paragraph_spacing}}",paragraph_spacing);
+
     let options = { format:'Letter'};
-    pdf.create(html,options).toFile(__dirname+'/public/resumes/resume.pdf',(err,res) => {
+    pdf.create(html,options).toFile(__dirname+'/public/resumes/resume.pdf',(err,response) => {
         let responseData = {};
         if(err) responseData = {error:"pdf error"};
-        else responseData["pdf_file"] = "https://resume-builder-node.herokuapp.com/resumes/resume.pdf";
+        else responseData["pdf_file"] = `${server_url}/resumes/resume.pdf`;
 
 
         const blob = htmlDocx.asBlob(html,{orientation:'landscape',margins:{top:720}});
         fs.createWriteStream(__dirname+'/public/resumes/resume.docx').write(blob);
-        responseData["docx_file"] = "https://resume-builder-node.herokuapp.com/resumes/resume.docx";
+        responseData["docx_file"] = `${server_url}/resumes/resume.docx`;
 
-        response.end(JSON.stringify(responseData));
+        res.json(responseData);
     });    
 })
 
@@ -62,5 +85,5 @@ let port = process.env.PORT || 8080;
 let server = app.listen(port,function(){
     let host = server.address().address
     let port = server.address().port
-    console.log("App listening at http://127.0.0.1:"+port);
+    console.log(`App listening at ${server_url}`);
 })
